b26c17f62a3c169641620cbf3f7f8126487dbc5e
Thong Huynh <thongh@coutureconsulting.com>
2018-10-10 19:04:24 +0700

    up

--- src/main/java/restReturnBO/PosAPI.java
+++ src/main/java/restReturnBO/PosAPI.java
@@ -1,6 +1,7 @@
 package restReturnBO;
 
 import java.io.BufferedReader;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -8,12 +9,21 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.security.cert.X509Certificate;
+import java.security.NoSuchAlgorithmException;
+import java.security.KeyManagementException;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class PosAPI {
 
 	public static void main(String[] args) {
 		try {
-			callREST("", "", "", "", "");
+			callREST("https://192.168.100.64:8082/pos-api/", "inventories/warehouses", "GET", "bpmapiuser", "APiU53rBpM");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -22,7 +32,7 @@ public class PosAPI {
 	
 	public static String callREST(String restEndpoint, String restResource, String method,
 			String username, String password) throws IOException {
-		String response = "";
+	    disableSslVerification();
 		StringBuffer stringBuffer = new StringBuffer();
 		URL url = null;
 		try {
@@ -68,31 +78,55 @@ public class PosAPI {
 					(conn.getInputStream())));
 			
 			String output;
-//			output = br.readLine();
-			
-//			System.out.println("Output from Server .... \n");
 			while ((output = br.readLine()) != null) {
-//				System.out.println(output);
-				response = output;
 				stringBuffer.append(output);
-			}
-			
-			
-			
-			conn.disconnect();
-			
+			}						
+			conn.disconnect();			
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-
 			e.printStackTrace();
-
-		}
-				
-		System.out.println(stringBuffer.toString());
-		System.out.println(response);
+		}				
+//		System.out.println(stringBuffer.toString());
+//		System.out.println(response);
 		return stringBuffer.toString();
 	}
+	
+	private static void disableSslVerification() {
+        try
+        {
+            // Create a trust manager that does not validate certificate chains
+            TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }
+            };
+
+            // Install the all-trusting trust manager
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            // Create all-trusting host name verifier
+            HostnameVerifier allHostsValid = new HostnameVerifier() {
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            };
+
+            // Install the all-trusting host verifier
+            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
+    }
 	
 }
